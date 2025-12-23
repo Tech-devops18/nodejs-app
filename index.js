48ae1921e1d93db74d357c17363389bfb54aa923
Tech-devops18 <irfancareers18@gmail.com>
2025-12-23 11:04:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Node.js app deployed using Jenkins Pipeline + Ansible playbook');
+  res.send('Node.js app deployed using Jenkins Pipeline + Ansible playbook + docker');
 });
 
 app.listen(3000, () => {
